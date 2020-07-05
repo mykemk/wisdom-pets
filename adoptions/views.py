bab8b13e1d97eaa9e54ee53968e617a5e47d99cf
Myke <mykemitrichev@gmail.com>
2020-07-05 13:19:39 +0300

    Added templates

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render,HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 def home(request):
-    return HttpResponse('<p>Welcome home</p>')
-
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {
+        'pets':pets
+    })
 def pet_detail(request,pet_id):
-    return HttpResponse(f'<p>Details for pet id {pet_id}</p>')
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Page not Found')
+    return render(request,'pet_detail.html',{
+                  'pet':pet
+                  })
     
